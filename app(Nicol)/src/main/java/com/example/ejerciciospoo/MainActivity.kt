7cdb24df0f570f060b802d04f9e3b54b74d543f3
nikki-021 <naime14paidican@gmail.com>
2024-04-20 14:21:19 -0300

    Actualizo mi parte

--- app(Nicol)/src/main/java/com/example/ejerciciospoo/MainActivity.kt
+++ app(Nicol)/src/main/java/com/example/ejerciciospoo/MainActivity.kt
@@ -16,4 +16,12 @@ class MainActivity : AppCompatActivity() {
     val ordenador = Ordenador("HP", "Pavilion", Estado.NUEVO, "Intel i5", 8)
     val tableta = Tableta("Samsung", "Galaxy Tab", Estado.USADO, 10.1)
     val smartphone = Smartphone("Apple", "iPhone 12", Estado.EN_REPARACION, true)
+
+    RegistroDispositivos.agregarDispositivo(ordenador)
+    RegistroDispositivos.agregarDispositivo(tableta)
+    RegistroDispositivos.agregarDispositivo(smartphone)
+
+    RegistroDispositivos.buscarDispositivo("HP", "Pavilion")?.mostrarDetalle()
+    RegistroDispositivos.buscarDispositivo("Samsung", "Galaxy Tab")?.mostrarDetalle()
+    RegistroDispositivos.buscarDispositivo("Apple", "iPhone 12")?.mostrarDetalle()
 }
